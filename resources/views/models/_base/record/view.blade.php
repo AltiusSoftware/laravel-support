aa3b10323794ac2d6363f2b109f167ea91e1194e
Hari Kant <hkant@pizzaforno.com>
2022-06-04 14:49:41 -0400

    Push grid view!

--- resources/views/models/_base/record/view.blade.php
+++ resources/views/models/_base/record/view.blade.php
@@ -1,38 +1,34 @@
 <x-layout>
 
-<x-portal type="blue" class="w-1/2">
-        <x-portal.header>
 
+<div class="grid grid-cols-2 gap-2">
+    <x-portal type="blue" class="w-full">
+            <x-portal.header>
         
-            {{ $record->singular}}: {{ $record->summary }}
-            <x-portal.actions>
-                                @include($record->view('record.actions'))
+                {{ $record->singular}}: {{ $record->summary }}
+                <x-portal.actions>
+                                    @include($record->view('record.actions'))
 
-            </x-portal.actions>
-        </x-portal.header>
-        <table class="table table-md w-full">
-            @include('altius::fields.table.rows', ['fields'=>$record->getFields()->setValues($record)])
-        </table>
-</x-portal>
-
-
-
-    
-@foreach($record->getChildrenInfo() as $c)
-
-    <x-portal type="blue" class="w-1/2">
-        <x-portal.header>
-                {{ $c->new->plural }}
-            <x-portal.actions>
-                <a class="btn btn-blue" href="{{ $c->new->routeAll()}}">View {{ $c->new->plural }}</a>
-            </x-portal.actions>
-        </x-portal.header>
-        
-   </x-portal>
-
- 
+                </x-portal.actions>
+            </x-portal.header>
+            <table class="table table-md w-full">
+                @include('altius::fields.table.rows', ['fields'=>$record->getFields()->setValues($record)])
+            </table>
+    </x-portal>
    
+    @foreach($record->getChildrenInfo() as $c)
 
-@endforeach
+        <x-portal type="blue" class="w-full">
+            <x-portal.header>
+                    {{ $c->new->plural }}
+                <x-portal.actions>
+                    <a class="btn btn-xs btn-blue" href="{{ $c->new->routeAll()}}">View {{ $c->new->plural }}</a>
+                </x-portal.actions>
+            </x-portal.header>
+            
+    </x-portal>
+    @endforeach
+    @include($record->view('record.grid'))
+</div>
     
 </x-layout>
